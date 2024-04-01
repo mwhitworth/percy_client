69cc0af777ea0c473992f52018ac746e0d69dd8a
Matt Whitworth <mwhitworth@gmail.com>
2024-04-01 15:00:43 +0100

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PercyClient.MixProject do
   def project do
     [
       app: :percy_client,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       preferred_cli_env: [ci: :test],
